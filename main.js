6ba887fd37fc71725f31fbcc494ffccde3b33053
SarahABusch <renata_gomes88@hotmail.com>
2025-06-02 09:16:14 -0300

    ciação do main.js e criação da função para adicionar itens

---
+++ main.js
@@ -0,0 +1,12 @@
+const items = []
+
+function addItem () {
+    const itemName = document.querySelector("#item").value
+   const item = {
+    name: itemName,
+    checked: false 
+   }
+
+   items.push(item)
+   document.querySelector("#item").value = ""
+}
